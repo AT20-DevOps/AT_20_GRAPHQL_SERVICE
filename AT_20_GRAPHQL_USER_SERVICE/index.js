c3f09b89808ecb97058dabd3d429fd0fe149bec5
Percy Rodrigo Fernandez Villarroel <fernandezvillarroel@gmail.com>
2023-05-19 08:45:49 -0400

    clean files

--- AT_20_GRAPHQL_USER_SERVICE/index.js
+++
@@ -1,6 +0,0 @@
-import {startApolloServer} from './app.js';
-import { typeDefs } from './graphql/typeDefs.js';
-import { resolvers } from './graphql/resolvers.js';
-//import { connectDB } from './db.js';
-
-startApolloServer(typeDefs, resolvers);
